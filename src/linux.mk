90980c65f316272ccaf42815b0e50c15745a91b1
Philip Papadopoulos <ppapadopoulos@ucsd.edu>
2012-05-13 19:25:19 +0000

    Updating method for building, updating of modules when you have updated the kernel, documentation

--- src/linux.mk
+++ src/linux.mk
@@ -1,4 +1,13 @@
 SRCDIRS = `find * -prune\
 	  -type d 	\
 	  ! -name CVS	\
-	  ! -name .`
+	  ! -name . \
+	  ! -name zfs \
+	  ! -name spl \
+	  ! -name zfs-source-modules`
+
+ifndef ZFS-SOURCE-ONLY
+# Order of Build is important: zfs-source-modules includes the SRPMS built
+# by spl and zfs
+SRCDIRS += spl zfs zfs-source-modules
+endif 
---
+++ src/zfs-source-roll/version.mk
@@ -0,0 +1,5 @@
+NAME    	= zfs-source-roll
+VERSION 	= 0.6.0
+RC		= -rc8
+RELEASE 	= 1
+TARBALL_POSTFIX	= tar.gz
